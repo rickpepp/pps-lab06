e4d60ebc6328bc805c9ecdaf5a08e3b288d2107e
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2025-04-12 18:28:43 +0200

    Created interface ConferenceReviewing and relative test, then implemented orderedScores and loadReview

--- src/main/scala/ex2/ConferenceReviewing.scala
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -1,73 +1,93 @@
 package ex2
 
+import scala.:+
+import scala.collection.immutable
+
+
+enum Question:
+  case RELEVANCE // ("È importante per questa conferenza?"),
+  case SIGNIFICANCE // ("Produce contributo scientifico?"),
+  case CONFIDENCE // ("Ti senti competente a commentarlo?");
+  case FINAL // ("É un articolo da accettare?")
+
 /**
  * An interface modelling the results of reviewing articles of a conference
  * Each reviewer (revisore) reads an article (articolo), and answers to a number of questions
  * with a score from 0 (bad) to 10 (excellent).
  * Note that each article can be reviewed by many reviewers (typically, from 2 to 4), but the
  * system does not keep track of the identity of reviewers
- *
- *
- * public interface ConferenceReviewing {
- *
- * /**
- * * For each article, the reviewer has to reply to all the following questions
- * */
- * enum Question {
- * RELEVANCE,       // ("È importante per questa conferenza?"),
- * SIGNIFICANCE, 	// ("Produce contributo scientifico?"),
- * CONFIDENCE,   	// ("Ti senti competente a commentarlo?");
- * FINAL;        	// ("É un articolo da accettare?")
- * }
- *
- * /**
- * * @param article
- * * @param scores
- * * loads a review for the specified article, with complete scores as a map
- * */
- * void loadReview(int article, Map<Question,Integer> scores);
- *
- * /**
- * * @param article
- * * @param relevance
- * * @param significance
- * * @param confidence
- * * @param fin
- * * loads a review for the specified article, with the 4 explicit scores
- * */
- * void loadReview(int article, int relevance, int significance, int confidence, int fin);
- *
- * /**
- * * @param article
- * * @param question
- * * @return the scores given to the specified article and specified question, as an (ascending-ordered) list
- * */
- * List<Integer> orderedScores(int article, Question question);
- *
- * /**
- * * @param article
- * * @return the average score to question FINAL taken by the specified article
- * */
- * double averageFinalScore(int article);
- *
- * /**
- * * An article is considered accept if its averageFinalScore (not weighted) is > 5,
- * * and at least one RELEVANCE score that is >= 8.
- * * @return the set of accepted articles
- * */
- * Set<Integer> acceptedArticles();
- *
- *
- * /**
- * * @return accepted articles as a list of pairs article+averageFinalScore, ordered from worst to best based on averageFinalScore
- * */
- * List<Pair<Integer,Double>> sortedAcceptedArticles();
- *
- * /**
- * * @return a map from articles to their average "weighted final score", namely,
- * * the average value of CONFIDENCE*FINAL/10
- * * Note: this method is optional in this exam
- * */
- * Map<Integer, Double> averageWeightedFinalScoreMap();
- * }
  */
+trait ConferenceReviewing:
+  /**
+   * * @param article
+   * * @param scores
+   * * loads a review for the specified article, with complete scores as a map
+   * */
+  def loadReview(article: Int, scores: Map[Question, Int]): Unit
+
+  /**
+   * * @param article
+   * * @param relevance
+   * * @param significance
+   * * @param confidence
+   * * @param fin
+   * * loads a review for the specified article, with the 4 explicit scores
+   * */
+  def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit
+
+  /**
+   * * @param article
+   * * @param question
+   * * @return the scores given to the specified article and specified question, as an (ascending-ordered) list
+   * */
+  def orderedScores(article: Int, question: Question): List[Int]
+
+  /**
+   * * @param article
+   * * @return the average score to question FINAL taken by the specified article
+   * */
+  def averageFinalScore(article: Int): Double
+
+  /**
+   * * An article is considered accept if its averageFinalScore (not weighted) is > 5,
+   * * and at least one RELEVANCE score that is >= 8.
+   * * @return the set of accepted articles
+   * */
+  def acceptedArticles(): Set[Int]
+
+  /**
+   * * @return accepted articles as a list of pairs article+averageFinalScore, ordered from worst to best based on averageFinalScore
+   * */
+  def sortedAcceptedArticles(): List[(Int, Double)]
+
+  /**
+   * * @return a map from articles to their average "weighted final score", namely,
+   * * the average value of CONFIDENCE*FINAL/10
+   * * Note: this method is optional in this exam
+   * */
+  def averageWeightedFinalScoreMap(): Map[Int, Double]
+
+  /**
+   * For each article, the reviewer has to reply to all the following questions
+   */
+
+
+object ConferenceReviewing:
+  def apply(): ConferenceReviewing = ConferenceReviewingImpl()
+
+  private class ConferenceReviewingImpl extends ConferenceReviewing:
+    private var reviews: List[(Int, Map[Question, Int])] = List()
+    def loadReview(article: Int, scores: Map[Question, Int]): Unit =
+      reviews = reviews :+ (article, scores)
+    def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit =
+      loadReview(article,
+        Map(Question.RELEVANCE -> relevance,
+          Question.CONFIDENCE -> confidence,
+          Question.SIGNIFICANCE -> significance,
+          Question.FINAL -> fin))
+    def orderedScores(article: Int, question: Question): List[Int] =
+      reviews.filter(_._1 == article).map(_._2(question)).sorted
+    def averageFinalScore(article: Int): Double = ???
+    def acceptedArticles(): Set[Int] = ???
+    def sortedAcceptedArticles(): List[(Int, Double)] = ???
+    def averageWeightedFinalScoreMap(): Map[Int, Double] = ???
--- src/test/scala/ex2/ConferenceReviewingTest.scala
+++ src/test/scala/ex2/ConferenceReviewingTest.scala
@@ -2,6 +2,9 @@ package ex2
 
 import org.junit.*
 import org.junit.Assert.*
+
+import scala.collection.mutable
+
 /**
  * Si consulti la documentazione dell'interfaccia ConferenceReviewing, che modella i risultati del processo di revisione
  * degli articoli di una conferenza. Ogni articolo viene da revisionato da uno o più revisori anonimi, ognuno dei quali fornisce
@@ -24,12 +27,11 @@ import org.junit.Assert.*
  * Si tolga il commento al codice del test.
  */
 
-class Test:
-  private val cr = Unit
+class ConferenceReviewingTest:
+  private var cr: ConferenceReviewing = ConferenceReviewing()
 
   @Before
-  /*def init(): Unit =
-    cr = ConferenceReviewing()
+  def init(): Unit =
     cr.loadReview(1, 8, 8, 6, 8); // 4.8 è il voto finale pesato (usato da averageWeightedFinalScoreMap)
     cr.loadReview(1, 9, 9, 6, 9); // 5.4
     cr.loadReview(2, 9, 9, 10, 9); // 9.0
@@ -38,24 +40,25 @@ class Test:
     cr.loadReview(3, 4, 4, 4, 4); // 1.6
     cr.loadReview(4, 6, 6, 6, 6); // 3.6
     cr.loadReview(4, 7, 7, 8, 7); // 5.6
-    val map: Map[Question, Int] = new EnumMap<>(Question.class);
-    map.put(Question.RELEVANCE, 8)
-		map.put(Question.SIGNIFICANCE, 8)
-		map.put(Question.CONFIDENCE, 7)// 5.6
-		map.put(Question.FINAL, 8)
-		cr.loadReview(4, map)
-		cr.loadReview(5, 6, 6, 6, 10)// 6.0
-		cr.loadReview(5, 7, 7, 7, 10)// 7.0
-		
-	@Test
+    val map: Map[Question, Int] = Map(
+      Question.RELEVANCE -> 8,
+      Question.SIGNIFICANCE -> 8,
+      Question.CONFIDENCE -> 7,
+      Question.FINAL -> 8
+    )
+    cr.loadReview(4, map)
+    cr.loadReview(5, 6, 6, 6, 10)// 6.0
+    cr.loadReview(5, 7, 7, 7, 10)// 7.0
+
+  @Test
   def testOrderedScores(): Unit =
     // l'articolo 2 ha preso su RELEVANCE i due voti 4,9
-    assertEquals(cr.orderedScores(2, Question.RELEVANCE), Arrays.asList(4, 9));
+    assertEquals(cr.orderedScores(2, Question.RELEVANCE), List(4, 9));
     // e simile per gli altri
-    assertEquals(cr.orderedScores(4, Question.CONFIDENCE), Arrays.asList(6, 7, 8));
-    assertEquals(cr.orderedScores(5, Question.FINAL), Arrays.asList(10, 10));
+    assertEquals(cr.orderedScores(4, Question.CONFIDENCE), List(6, 7, 8));
+    assertEquals(cr.orderedScores(5, Question.FINAL), List(10, 10));
     
-  @Test
+  /*@Test
   def testAverageFinalScore(): Unit =
     // l'articolo 1 ha preso voto medio su FINAL pari a 8.5, con scarto massimo 0.01
     assertEquals(cr.averageFinalScore(1), 8.5, 0.01);
